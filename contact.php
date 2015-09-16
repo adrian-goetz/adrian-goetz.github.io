7848c851cb20afc77608328d70f01b00bbc7670f
Adrian Goetz <adrian.goetz@gmail.com>
2015-09-16 17:53:31 -0400

    working on contact file

--- contact.php
+++ contact.php
@@ -1,9 +1,38 @@
 <html>
 <head>
-<title>My Very First Contact Form</title>
+<title>Contact Form</title>
 </head>
-
 <body>
+	<form name="form1" methond="post" action="send.php">
+
+		<ul>
+			<li>
+				<label for="name">Name:</label>
+				<input type="text" name="name" id="name" />
+			</li>
+			<li>
+				<label for="email">Email:</label>
+				<input type="text" name="email" id="email" />
+			</li>
+			<li>
+				<label for="topic">Topic:</label>
+				<select>
+					<option value="One">One</option>
+					<option value="Two">Two</option>
+					<option value="Three">Three</option>
+					<option value="Four">Four</option>
+				</select>
+			</li>
+			<li>
+				<label for="message">Message:</label>
+				<textarea id="message" name="message" cols="42" rows="9"></textarea>
+			</li>
+			<li>
+				<input type="submit" value="Submit" />
+			</li>
+		</ul>
+
+	</form>
 
 </body>
 
